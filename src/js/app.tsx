b37559b45a3c0c7500e8af93a38de25273d803c3
Jakub Kowalski <contact@jakubkowalski.dev>
2022-07-30 16:08:25 +0200

    🥷Feat: Components, Templates Structure

--- src/js/app.tsx
+++ src/js/app.tsx
@@ -2,5 +2,8 @@ import '../scss/app.scss';
 
 /* Your JS Code goes here */
 
-/* Demo JS */
-import './demo.tsx';
+/* Components JS */
+import './components/index.tsx';
+
+/* Templates JS */
+import './templates/index.tsx';
