25ce309c76b452bdcaf32f4717093e10b1423174
Jakub Kowalski <contact@jakubkowalski.dev>
2022-08-02 23:07:00 +0200

    🥷Feat: Background, Decorator, Some changes

--- src/js/app.tsx
+++ src/js/app.tsx
@@ -2,6 +2,8 @@ import '../scss/app.scss';
 
 /* Your JS Code goes here */
 
+// I will need to memoize data https://www.npmjs.com/package/p-memoize
+
 /* Components JS */
 import './components/index.tsx';
 
