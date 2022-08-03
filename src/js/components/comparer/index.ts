037884a4169ac8c12dde8508c81995c13e3c5cc7
Jakub Kowalski <contact@jakubkowalski.dev>
2022-08-03 17:04:12 +0200

    🥷Feat: Added Search Bar

--- src/js/components/comparer/index.ts
+++ src/js/components/comparer/index.ts
@@ -1,4 +1,4 @@
 import insertCurrentDate from './utilities';
 
 const infoBoxDate = document.querySelector('.comparer__info-box-date') as HTMLElement;
-infoBoxDate.textContent = insertCurrentDate();
+infoBoxDate.textContent = `${insertCurrentDate()}, `;
