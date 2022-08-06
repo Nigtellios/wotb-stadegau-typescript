d93301e1c5559a1451e667e2ab720e4963195384
Jakub Kowalski <contact@jakubkowalski.dev>
2022-08-06 14:28:49 +0200

    🥷Feat: Save tanks collection into local storage

--- src/js/components/comparer/controller.ts
+++ src/js/components/comparer/controller.ts
@@ -1,10 +1,6 @@
-import BuildTanksList from './classes/BuildTanksList';
 import SearchBar from './classes/SearchBar';
 
 document.addEventListener('DOMContentLoaded', async () => {
-  const buildTanksList = new BuildTanksList();
-  await buildTanksList.getList();
-
   const searchBar = new SearchBar();
-  searchBar.init();
+  await searchBar.init();
 });
